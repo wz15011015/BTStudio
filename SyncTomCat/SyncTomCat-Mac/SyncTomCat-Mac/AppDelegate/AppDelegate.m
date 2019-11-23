3219610cf2525a2418893b42c7e28ca74e9ffbb7
wangzhi <wzqsyk@126.com>
2019-11-23 10:36:18 +0800

    SyncTomCat-Mac： 点击关闭按钮时，退出应用程序。

--- SyncTomCat/SyncTomCat-Mac/SyncTomCat-Mac/AppDelegate/AppDelegate.m
+++ SyncTomCat/SyncTomCat-Mac/SyncTomCat-Mac/AppDelegate/AppDelegate.m
@@ -16,12 +16,22 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    // 1. 点击关闭按钮时,直接让应用程序退出
+    // 注册 NSWindowWillCloseNotification 通知,然后在通知方法中退出应用程序
+    NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+    [defaultCenter addObserver:self selector:@selector(applicationWindowWillClose:) name:NSWindowWillCloseNotification object:nil];
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+
+#pragma mark - Custom Notification
+
+- (void)applicationWindowWillClose:(NSNotification *)aNotification {
+    [NSApp terminate:self];
+}
 
 @end
