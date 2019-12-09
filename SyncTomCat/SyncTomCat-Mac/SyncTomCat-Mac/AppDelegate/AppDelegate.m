ae85a89a3da60efbd00100eaefe4ec8693c60fdc
wangzhi <wzqsyk@126.com>
2019-12-09 09:42:31 +0800

    SyncTomCat-Mac:    -- 应用程序关闭功能的优化。

--- SyncTomCat/SyncTomCat-Mac/SyncTomCat-Mac/AppDelegate/AppDelegate.m
+++ SyncTomCat/SyncTomCat-Mac/SyncTomCat-Mac/AppDelegate/AppDelegate.m
@@ -17,8 +17,7 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
     
-    // 1. 点击关闭按钮时,直接让应用程序退出
-    // 注册 NSWindowWillCloseNotification 通知,然后在通知方法中退出应用程序
+    // 注册通知
     NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
     [defaultCenter addObserver:self selector:@selector(applicationWindowWillClose:) name:NSWindowWillCloseNotification object:nil];
 }
@@ -27,11 +26,19 @@
     // Insert code here to tear down your application
 }
 
+/// 当关闭最后一个窗口时,退出应用程序
+/// @param sender 应用程序
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
 
 #pragma mark - Custom Notification
 
+/// 关闭窗口的通知 (注意: 关闭任意窗口时都会收到通知)
+/// @param aNotification 通知
 - (void)applicationWindowWillClose:(NSNotification *)aNotification {
-    [NSApp terminate:self];
+//    [NSApp terminate:self];
 }
 
 @end
